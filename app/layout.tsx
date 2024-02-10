6d3c51631aa0f4468557a59c37d61408b5f494e3
Pranjal Gupta <pranjalg1122@gmail.com>
2024-02-10 22:41:32 +0530

    feat: added ToastContainer in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
+import { Slide, ToastContainer } from "react-toastify";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Poppins({
-  weight: ["100", "200", "300", "400", "500"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -21,9 +23,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-background-700 text-text-200"}>
-        {children}
-        <ToastContainer />
+      <body
+        className={
+          inter.className +
+          " bg-background-700 text-text-50 min-h-screen w-screen flex flex-col items-center flex-grow px-2"
+        }
+      >
+        <Navbar />
+        <div className="min-h-full flex flex-grow w-full items-center justify-center">
+          {children}
+        </div>
+        <Footer />
+        <ToastContainer
+          draggable={false}
+          hideProgressBar={true}
+          newestOnTop={true}
+          theme={"colored"}
+          autoClose={3000}
+          pauseOnFocusLoss={true}
+          limit={3}
+          pauseOnHover={true}
+          position="top-right"
+          transition={Slide}
+          icon={false}
+        />
       </body>
     </html>
   );
