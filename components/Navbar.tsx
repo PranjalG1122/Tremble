acc64abed45948bf9caf42e9c28de19be1287f84
Pranjal Gupta <pranjalg1122@gmail.com>
2024-02-10 22:41:46 +0530

    style: Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,20 @@
+import Image from "next/image";
+import { Heading } from "./Heading";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex flex-row items-center justify-between w-full px-16 py-2">
+      <div className="flex flex-row items-center gap-2">
+        <Image
+          src="/tremble.png"
+          alt="Tremble"
+          width={32}
+          height={32}
+          unoptimized={true}
+        />
+        <Heading size="l">Tremble</Heading>
+      </div>
+      <div>Documentation</div>
+    </nav>
+  );
 }
